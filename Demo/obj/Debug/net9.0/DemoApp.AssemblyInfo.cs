4a638d02323b2f41957d61b694b4b314bacaeeeb
Josef Nobach <josef.nobach@outlook.com>
2025-11-17 08:17:22 +0100

    Refactor code structure for improved readability and maintainability

--- Demo/obj/Debug/net9.0/DemoApp.AssemblyInfo.cs
+++ Demo/obj/Debug/net9.0/DemoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daff240b78cd2a7bb15eff78df5bf64b3ae9c063")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5e55d3324c10625f0afab8fa5cdd1ad39f78d72")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
