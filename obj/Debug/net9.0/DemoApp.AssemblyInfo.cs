f3020d180ece0e969b85164baeef1cd3c26dbce8
Josef Nobach <josef.nobach@outlook.com>
2025-11-22 13:29:31 +0100

    Modify CustomTitleBar.axaml and update associated build artifacts.

--- obj/Debug/net9.0/DemoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/DemoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e8024493392ee9ff9b8aa14938a18a2135385c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8d79c85259cdf41fd3e0b4528f18803a4987511")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
